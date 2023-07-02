08d82f75c2e7c63afa5870e9dfe91461b4866c0d
Сергей Конников <s.konnikov@yandex.ru>
2023-07-02 15:58:53 +1100

    обновил permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,58 +4,22 @@ from rest_framework import permissions
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-            request.method in permissions.SAFE_METHODS
-            or (request.user.is_authenticated and request.user.is_admin)
+            request.method in permissions.SAFE_METHODS or
+            (request.user.is_authenticated and request.user.is_admin)
         )
 
 
-class IsAuthorOrModeratorOrAdminOrReadOnly(permissions.BasePermission):
+class IsAuthorOrModeratorOrAdminOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
     def has_object_permission(self, request, view, obj):
         return (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-            or request.user.is_moderator
-            or request.user.is_admin
+            request.method in permissions.SAFE_METHODS or
+            obj.author == request.user or
+            request.user.is_moderator or
+            request.user.is_admin
         )
 
 
 class IsAdmin(permissions.BasePermission):
-    def has_permission(self, request, view):
+    @staticmethod
+    def has_permission(request, view):
         return request.user.is_authenticated and request.user.is_admin
-
-
-class IsSuperuser(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_superuser
-
-
-class IsAuthenticatedOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
-
-
-class IsUserOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or obj.user == request.user
-        )
-
-
-class IsModeratorOrAdmin(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated
-            and (request.user.is_moderator or request.user.is_admin)
-        )
-
-
-class IsAdminOrSuperuser(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated
-            and (request.user.is_admin or request.user.is_superuser)
-        )
