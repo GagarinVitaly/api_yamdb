f7474c45be17710af0bd83d2e5fb8252ae0f452e
GVU <vitalygagarin89@gmail.com>
2023-07-14 14:39:22 +1000

    serializers.py

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,3 +1,5 @@
+import datetime
+
 from rest_framework import serializers
 from django.core.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
@@ -7,14 +9,14 @@ from reviews.models import (
     Comment,
     Genre,
     Review,
-    Title,)
+    Title, )
 from users.constants import (
     MAX_LEN_EMAIL,
-    MAX_LEN_USERNAME,)
+    MAX_LEN_USERNAME, )
 from users.models import User
 from users.validators import (
     username_validator,
-    forbidden_usernames_validator,)
+    forbidden_usernames_validator, )
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -37,14 +39,15 @@ class SignUpSerializer(serializers.Serializer):
     username = serializers.CharField(
         max_length=MAX_LEN_USERNAME,
         required=True,
-        validators=[forbidden_usernames_validator, username_validator],)
+        validators=[forbidden_usernames_validator, username_validator], )
     email = serializers.EmailField(
         max_length=MAX_LEN_EMAIL,
-        required=True,)
+        required=True, )
 
     def validate(self, data):
-        """
-        Запрет на использование одинакового адреса электронной почты,
+        """Запрет на использование.
+
+        Одинакового адреса электронной почты,
         имени пользователя.
         """
 
@@ -66,9 +69,9 @@ class TokenSerializer(serializers.Serializer):
     username = serializers.CharField(
         max_length=MAX_LEN_USERNAME,
         required=True,
-        validators=[forbidden_usernames_validator, username_validator],)
+        validators=[forbidden_usernames_validator, username_validator], )
     confirmation_code = serializers.CharField(
-        required=True,)
+        required=True, )
 
 
 class UserProfileSerializer(UserSerializer):
@@ -98,50 +101,41 @@ class TitleSerializer(serializers.ModelSerializer):
 
     genre = GenreSerializer(many=True, read_only=True)
     category = CategorySerializer(read_only=True)
-    rating = serializers.FloatField(read_only=True)
+    rating = serializers.IntegerField(default=0, read_only=True)
 
     class Meta:
         model = Title
-        fields = ('id',
-                  'name',
-                  'year',
-                  'description',
-                  'genre',
-                  'category',
-                  'rating')
+        fields = ('id', 'name', 'year', 'rating',
+                  'description', 'genre', 'category')
 
 
-class TitleCreateSerializer(serializers.ModelSerializer):
+class TitleCreateSerializer(TitleSerializer):
     """Сериализатор для создания и обновления произведений."""
 
     genre = serializers.SlugRelatedField(
         many=True,
         slug_field='slug',
-        queryset=Genre.objects.all())
+        queryset=Genre.objects.all()
+    )
     category = serializers.SlugRelatedField(
         slug_field='slug',
-        queryset=Category.objects.all())
-    rating = serializers.FloatField(read_only=True)
+        queryset=Category.objects.all()
+    )
+    rating = serializers.IntegerField(default=0, read_only=True)
 
     class Meta:
         model = Title
-        fields = ('id',
-                  'name',
-                  'year',
-                  'description',
-                  'genre',
-                  'category',
-                  'rating')
+        fields = ('id', 'name', 'year', 'rating',
+                  'description', 'genre', 'category')
 
     def validate(self, data):
+        current_year = datetime.date.today().year
+        year = data.get('year')
+        if year is not None and year > current_year:
+            raise serializers.ValidationError('Год выпуска не может быть больше текущего года.')
         if data.get('name') == data.get('category'):
-            raise serializers.ValidationError(
-                'Название не должно совпадать с категорией.')
+            raise serializers.ValidationError('Название не должно совпадать с категорией.')
         return data
-
-    def to_representation(self, instance):
-        serializer = TitleSerializer(instance)
-        return serializer.data
 
 
 class ReviewSerializer(serializers.ModelSerializer):
